d1c1cd922cceec8cb5cd181ff46b7d7e7cb0da66
Doug Enas <douglassenas@gmail.com>
2019-05-21 13:46:06 -0400

    changed tweet char limit to 140

--- twitterclone/tweet/forms.py
+++ twitterclone/tweet/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class TweetForm(forms.Form):
-    body = forms.CharField(widget=forms.Textarea, max_length=240)
+    body = forms.CharField(widget=forms.Textarea, max_length=140)
